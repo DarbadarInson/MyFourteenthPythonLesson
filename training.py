6a2d2465cbdc040b772aed4fac70acb530829076
Ozodbek <devanabuildmuber@gmail.com>
2023-12-25 16:05:41 -0500

    this is my Python Lesson 14

---
+++ training.py
@@ -0,0 +1,39 @@
+def myfunction():
+  return 3+3
+  print("Hello, World!")
+
+print(myfunction()) 
+
+print("------------------------------------")
+
+num = [1, 3, 5, 7, 9, 11]
+
+sum1 = sum (num)
+
+print (sum1)
+
+sum1 = sum (num, 13)
+
+print (sum1)
+
+print("------------------------------------")
+
+x = range(5)
+for n in x:
+  print(n) 
+
+print("------------------------------------")
+
+x = range(5, 11)
+for n in x:
+  print(n) 
+
+print("------------------------------------")
+
+for num in range(10,21,2):
+  print(num)
+
+
+
+
+
